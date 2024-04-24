7b8227b0d7065e1675f4594dd95b5743f93fe7ff
LeSasse <l.sasse@fz-juelich.de>
2024-04-24 20:16:03 +0200

    tests added to mutant.rs

--- src/mutants.rs
+++ src/mutants.rs
@@ -6,6 +6,11 @@ use std::path::PathBuf;
 fn replacement_from_line(line: &str) -> Option<(String, String)> {
     match line {
         l if line.contains("+") => Some(("+".into(), "-".into())),
+        l if line.contains("-") => Some(("-".into(), "+".into())),
+        l if line.contains("*") => Some(("*".into(), "/".into())),
+        l if line.contains("/") => Some(("/".into(), "*".into())),
+        l if line.contains(" and ") => Some((" and ".into(), " or ".into())),
+        l if line.contains(" or ") => Some((" or ".into(), " and ".into())),
         _ => None,
     }
 }
@@ -27,7 +32,7 @@ fn add_mutants_from_file(mut mutant_vec: &Vec<Mutant>, path: &PathBuf) {
             Some((before, after)) => {
                 let mutant = Mutant {
                     file_path: path.clone(),
-                    line_number: line_nr,
+                    line_number: line_nr + 1,
                     before: before,
                     after: after,
                 };
@@ -45,6 +50,10 @@ pub fn find_mutants(glob_expression: &str) {
     for entry in glob(glob_expression).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
+                let path_str = path.clone().into_os_string().into_string().unwrap();
+                if path_str.contains("test") {
+                    continue;
+                }
                 add_mutants_from_file(&mut possible_mutants, &path);
             }
             Err(e) => println!("{}", e),
@@ -58,4 +67,41 @@ pub struct Mutant {
     line_number: usize,
     before: String,
     after: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mutants;
+
+    #[test]
+    fn test_replacement_from_line() {
+        let line = "print('Hello World')";
+        let option = mutants::replacement_from_line(&line);
+        println!("{:?}", option);
+        assert!(option.is_none(), "Expected the option to be None");
+
+        let line = "5 + 5";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), ("+".into(), "-".into()));
+
+        let line = "5 - 5";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), ("-".into(), "+".into()));
+
+        let line = "5 * 5";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), ("*".into(), "/".into()));
+
+        let line = "5 / 5";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), ("/".into(), "*".into()));
+
+        let line = "True and False";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), (" and ".into(), " or ".into()));
+
+        let line = "True or False";
+        let option = mutants::replacement_from_line(&line);
+        assert_eq!(option.unwrap(), (" or ".into(), " and ".into()));
+    }
 }
